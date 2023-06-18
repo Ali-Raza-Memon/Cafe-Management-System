f6a73b7125a529a9cc8c42f313ade350e90bd090
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-18 23:50:46 +0500

    Not importing EnableWebSecurity

---
+++ src/main/java/com/cafe/system/JWT/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.cafe.system.JWT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+
+public class SecurityConfig{
+}
