d07343769ee778bafe56e4b90c2d70f7dd4caef2
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-18 16:18:04 +0500

    named method derived in UserDao

--- src/main/java/com/cafe/system/constants/CafeConstants.java
+++ src/main/java/com/cafe/system/constants/CafeConstants.java
@@ -3,4 +3,5 @@ package com.cafe.system.constants;
 public class CafeConstants {
 
     public static final String SOMETHING_WENT_WRONG = "Something Went Wrong.";
+    public static final String INVALID_DATA = "Invalid_Data";
 }
--- src/main/java/com/cafe/system/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/cafe/system/serviceImpl/UserServiceImpl.java
@@ -1,11 +1,19 @@
 package com.cafe.system.serviceImpl;
 
+import com.cafe.system.constants.CafeConstants;
 import com.cafe.system.service.UserService;
+import com.cafe.system.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.bridge.MessageUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+import static org.hibernate.id.enhanced.StandardOptimizerDescriptor.log;
+
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -14,7 +22,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
 
+        MessageUtil log;
+        log.info("Inside signup {}",requestMap);
 
+        if(validateSignUpMap(requestMap)){
+
+        }else {
+            return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+        }
     }
 
     private boolean validateSignUpMap(Map<String,String> requestMap){
